38500252afab8682d1eaeb86abca48e92ff08e31
Dingle <Stepa202120212021@gmail.com>
2024-12-31 02:30:32 -0700

    Interactive Objects

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -16,6 +16,7 @@ public class Player extends Entity{
 
     public final int screenX;
     public final int screenY;
+    int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
@@ -27,6 +28,8 @@ public class Player extends Entity{
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
         solidArea.height = 32;
 
@@ -74,8 +77,9 @@ public class Player extends Entity{
             // Check tile collision
             collisionOn = false;
             gp.cChecker.checkTile(this);
+            int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
 
-            // If collision is false , player can move
             if (!collisionOn) {
                 switch (direction) {
                     case "up": worldY -= speed; break;
@@ -95,6 +99,16 @@ public class Player extends Entity{
                     spriteNumber = 1;
                 }
                 spriteCounter = 0;
+            }
+        }
+    }
+
+    public void pickUpObject(int i) {
+        if (i != 999) {
+            String objectName = gp.obj[i].name;
+            switch (objectName) {
+                case "Key": hasKey++; gp.obj[i] = null; System.out.println("Key: " + hasKey); break;
+                case "Door": if (hasKey > 0) {gp.obj[i] = null; hasKey--; System.out.println("Key: " + hasKey);} break;
             }
         }
     }
