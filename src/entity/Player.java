9516c0c772ed3d8414edd8c3ab9885ef7bf4defd
Dingle <Stepa202120212021@gmail.com>
2025-01-05 15:48:44 -0700

    New Texture (Send Help)

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -130,7 +130,7 @@ public class Player extends Entity{
                 case "Gate": if (hasKey > 0) {gp.playSE(3); gp.obj[i] = null; hasKey--; gp.ui.showMessage("-1 Key");} else {gp.ui.showMessage("Key Required");} break;
                 case "Boots": gp.playSE(2); speed += 1; gp.obj[i] = null; gp.ui.showMessage("+1 Speed"); break;
                 case  "Chest": gp.ui.gameEnd = true;
-                gp.playSE(4);
+                //gp.playSE(4);
                      break;
             }
         }
